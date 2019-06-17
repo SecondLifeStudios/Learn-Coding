b71a65bb83ec527cc06f09e17f13becaeb31358b
Marcel J <lprandosofficial@gmail.com>
2019-06-17 02:56:01 +0200

    + Arrow Member Selection Operator

---
+++ C++/Arrow Member Selection Operator/Arrow Member Selection Operator/Arrow Member Selection Operator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Practicing.h"
+
+int main()
+{
+	      
+	Practicing Output;                                          // Objekt 'Output'
+	Practicing* Output_P = &Output;                             // Pointer zeigt auf die Speicheradresse (Memory Adress) des Objektes 'Output'
+
+	std::cout << "Without pointer:" << std::endl;
+	Output.rText();                                             // Es wird vom Objekt aus auf die Funktion zugegriffen.
+
+	std::cout << std::endl;
+
+	std::cout << "With pointer:" << std::endl;
+	Output_P->rText();                                          /* Der 'Arrow Member Selection Operator' wird benötigt, um von dem Pointer aus auf
+																   eine Funktion einer Klasse zuzugreifen, anstelle von dem Objektes aus. 
+																   Es wird vom Pointer aus, welcher auf die Speicheradresse des Objektes 'Output' zeigt auf 
+																   die Funktion zugegriffen. Achtung: Es wird nur auf die Speicheradresse des Objekets zugegriffen,
+																   nicht auf die der Funktion. */
+
+}
+
---
+++ C++/Arrow Member Selection Operator/Arrow Member Selection Operator/Practicing.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Practicing.h"
+
+void Practicing::rText() {
+	std::cout << "i'm a text! i never code in german lol" << std::endl;
+}
